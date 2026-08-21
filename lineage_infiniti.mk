7c6c5925b287c6c70bfced6d0698623fdf32a88d
chandu078 <chandudyavanapelli03@gmail.com>
2026-08-21 06:58:33 +0000

    infiniti: Update from OOS 11.A.46
    Change-Id: Ic23b54b01c5911bb736d4e4da2b410f28ec5b4aa

--- lineage_infiniti.mk
+++ lineage_infiniti.mk
@@ -23,8 +23,8 @@ PRODUCT_MODEL := CPH2745
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="qssi_64-user 16 BP2A.250605.015 1782911146828 release-keys" \
-    BuildFingerprint=OnePlus/CPH2745IN/OP611FL1:16/BP2A.250605.015/B.R4T3.16cae21_dc209e_dc22f5:user/release-keys \
+    BuildDesc="qssi_64-user 16 BP2A.250605.015 1785498910198 release-keys" \
+    BuildFingerprint=OnePlus/CPH2745IN/OP611FL1:16/BP2A.250605.015/B.R4T3.50c6369-2bb93d1-2bb93d2:user/release-keys \
     DeviceName=OP611FL1 \
     DeviceProduct=CPH2745 \
     SystemDevice=OP611FL1 \
